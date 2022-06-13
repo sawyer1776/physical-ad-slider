f25a6fd2267a4c149afdd1dc35addf84f3027bfd
Clay Gibson <blackcreekproductions@gmail.com>
2022-06-13 09:57:30 -0400

    refactoring 1

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ const refreshBtn = document.querySelector('.btn-refresh');
 //Keeps track of where you are in the loop
 let counter = 1;
 
+//current and next slide
+let imgCurrent = '';
+let imgNext = '';
+
 ///// MAKE DYNAMIC /////
 // This array is the file name of each image to be put into the slider
 // images need to be in the img folder to work
@@ -47,67 +51,67 @@ const createImg = function (index) {
 	sliderContainer.appendChild(newImg);
 };
 
-//Loop that Rotates the images
-// Needs to already have a current and next image element to function
-const rotate = function () {
-	setTimeout(() => {
-		//Remove the element that was pushed out of the viewport last loop
-		//Has to be put here or it would disapear before the transition was over.
-		if (+counter > 1) {
-			document
-				.querySelector(`[data-id="${counter - 1}"]`)
-				.remove();
-		} else {
-			document
-				.querySelector(`[data-id="${images.length}"]`)
-				.remove();
-		}
-
-		//Select the elements
-
-		const imgCurrent = document.querySelector(
-			`[data-id="${counter}"]`
-		);
-
-		let imgNext = '';
-
-		////SET the next image variable
-
-		// in the middle of the loop
-		if (+counter < images.length) {
-			imgNext = document.querySelector(
-				`[data-id="${counter + 1}"]`
-			);
-		}
-		// end of the loop
-		if (+counter == images.length) {
-			imgNext = document.querySelector(`[data-id="1"]`);
-		}
-
-		/////////move both images left 1 space
-
-		imgCurrent.style.transform = 'translateX(-100vw)';
-		imgNext.style.transform = 'translateX(0vw)';
-
-		//add a new next image
-
-		if (+counter <= images.length - 2) {
-			createImg(+counter + 2);
-			counter++;
-		} else {
-			createImg(+counter - (images.length - 2));
-			if (+counter < images.length) {
-				counter++;
-			} else {
-				counter = 1;
-			}
-		}
-		rotate();
-	}, SLIDE_DURATION * 1000);
+const removeSlide = function () {
+	//during the loop; remove the previous
+	if (+counter > 1) {
+		document
+			.querySelector(`[data-id="${counter - 1}"]`)
+			.remove();
+	}
+	//when counter is at 1 remove the last image
+	if (+counter == 1) {
+		document
+			.querySelector(`[data-id="${images.length}"]`)
+			.remove();
+	}
 };
 
+const setSlides = function () {
+	//current slide is the same as the current counter
+	imgCurrent = document.querySelector(
+		`[data-id="${counter}"]`
+	);
+
+	//SET the next image variable
+	// in the loop; select next image
+	if (+counter < images.length) {
+		imgNext = document.querySelector(
+			`[data-id="${counter + 1}"]`
+		);
+	}
+	// end of the loop; select image 1
+	if (+counter == images.length) {
+		imgNext = document.querySelector(`[data-id="1"]`);
+	}
+};
+
+const moveSlides = function () {
+	imgCurrent.style.transform = 'translateX(-100vw)';
+	imgNext.style.transform = 'translateX(0vw)';
+};
+
+const insertNext = function () {
+	//Not to the end of the loop; creat the image at +2
+	if (+counter <= images.length - 2) {
+		createImg(+counter + 2);
+		counter++;
+	} else {
+		//near or at the end of the loop
+		createImg(+counter - (images.length - 2));
+		//set or reset the counter
+		if (+counter < images.length) {
+			counter++;
+		} else {
+			counter = 1;
+		}
+	}
+};
+
+//infinite loop rotatest through images
+// Needs to already have a current and next image element to function
+
 const renderTime = function (object) {
-	object.innerHTML = '';
+	clear(object);
 	if (object == slideNumber) {
 		slideNumber.innerHTML = SLIDE_DURATION;
 	}
@@ -124,7 +128,7 @@ function toggleFullscreen() {
 	if (!document.fullscreenElement) {
 		elem.requestFullscreen().catch((err) => {
 			alert(
-				`Error attempting to enable fullscreen mode: ${err.message} (${err.name})`
+				`Unable to enter fullscreen, check you settings: ${err.message} (${err.name})`
 			);
 		});
 	} else {
@@ -132,22 +136,69 @@ function toggleFullscreen() {
 	}
 }
 
+const clear = function (element) {
+	element.innerHTML = '';
+};
+
+const hideButtons = function () {
+	buttonContainer.classList.remove('move-to-front');
+	buttonContainer.classList.add('move-to-back');
+};
+const showButtons = function () {
+	buttonContainer.classList.remove('move-to-back');
+	buttonContainer.classList.add('move-to-front');
+};
+
+const addToggleBtn = function () {
+	let newBtn = document.createElement('button');
+	newBtn.setAttribute('class', 'btn btn-toggle');
+	newBtn.textContent = 'Toggle Full Screen';
+	document
+		.querySelector('.btn-div')
+		.insertAdjacentElement('afterbegin', newBtn);
+	newBtn.addEventListener('click', toggleFullscreen);
+};
+
+const showAndHide = function (e) {
+	if (e.code == 'KeyH') {
+		hideButtons();
+	} else {
+		showButtons();
+	}
+};
+
+const rotate = function () {
+	setTimeout(() => {
+		//remove the previous slide
+		removeSlide();
+
+		//set the current and next variables
+		setSlides();
+
+		//move both images left 1 space
+		moveSlides();
+
+		//add a new next image
+		insertNext();
+
+		//starts over, infinite loop
+		rotate();
+	}, SLIDE_DURATION * 1000);
+};
+
 // Starts the application
-const init = function () {
-	// created the first 2 images only the first one will start visible
+const start = function () {
+	// create current, next, and previous image
 	createImg(1);
 	createImg(2);
-	//This one is invisible but placed to avoid an error in the rotate loop
 	createImg(images.length);
-	//Adds the transition time to the .img class
 
-	//positions the first image in the viewport
+	//move the first image in the viewport
 	document
 		.querySelector(`[data-id="1"]`)
 		.classList.add('start');
 	//Pushes the buttons to the back layer
-	buttonContainer.classList.remove('move-to-front');
-	buttonContainer.classList.add('move-to-back');
+	hideButtons();
 
 	//makes full screen
 	toggleFullscreen();
@@ -158,30 +209,9 @@ const init = function () {
 	//delete the start slideshow btn
 	startBtn.remove();
 
-	//Add toggle full screen
-	let newBtn = document.createElement('button');
-	newBtn.setAttribute('class', 'btn btn-toggle');
-	newBtn.textContent = 'Toggle Full Screen';
-	document
-		.querySelector('.btn-div')
-		.insertAdjacentElement('afterbegin', newBtn);
-	newBtn.addEventListener('click', toggleFullscreen);
+	//Add toggle full screen btn
+	addToggleBtn();
 };
-
-//Initial setup sets the visible number equal to the config
-slideNumber.innerHTML = '';
-slideNumber.innerHTML = SLIDE_DURATION;
-transNumber.innerHTML = '';
-transNumber.innerHTML = TRANSITION_TIME;
-document.documentElement.style.setProperty(
-	'--TRANSITION-TIME',
-	`${TRANSITION_TIME * 1000}ms`
-);
-
-//Clickables
-
-//Start Slideshow
-startBtn.addEventListener('click', init);
 
 //refresh images
 //looks for new images creates an array based on what is in the folder
@@ -192,16 +222,15 @@ startBtn.addEventListener('click', init);
 //slide more
 const moreSlideClick = function () {
 	SLIDE_DURATION = SLIDE_DURATION + 0.5;
-	slideNumber.innerHTML = '';
+	clear(slideNumber);
 	slideNumber.innerHTML = SLIDE_DURATION;
 };
-moreSlide.addEventListener('click', moreSlideClick);
 
 //slide less
 const lessSlideClick = function () {
 	if (SLIDE_DURATION > TRANSITION_TIME + 0.75) {
 		SLIDE_DURATION = SLIDE_DURATION - 0.5;
-		slideNumber.innerHTML = '';
+		clear(slideNumber);
 		slideNumber.innerHTML = SLIDE_DURATION;
 	} else {
 		alert(
@@ -209,36 +238,28 @@ const lessSlideClick = function () {
 		);
 	}
 };
-lessSlide.addEventListener('click', lessSlideClick);
+
 //slide more
 const moreTransClick = function () {
 	if (TRANSITION_TIME < SLIDE_DURATION - 0.5) {
 		TRANSITION_TIME = TRANSITION_TIME + 0.25;
-		transNumber.innerHTML = '';
+		clear(transNumber);
 		transNumber.innerHTML = TRANSITION_TIME;
 		document.documentElement.style.setProperty(
 			'--TRANSITION-TIME',
 			`${TRANSITION_TIME * 1000}ms`
 		);
-
-		// document.styleSheets[0].insertRule(
-		// 	`.img {transition: all ${
-		// 		TRANSITION_TIME * 1000
-		// 	}ms ease-in-out;}`,
-		// 	2
-		// );
 	} else {
 		alert(
 			'Transition time must be less than slide duration'
 		);
 	}
 };
-moreTrans.addEventListener('click', moreTransClick);
 
 //slide less
 const lessTransClick = function () {
 	TRANSITION_TIME = TRANSITION_TIME - 0.25;
-	transNumber.innerHTML = '';
+	clear(transNumber);
 	transNumber.innerHTML = TRANSITION_TIME;
 
 	document.documentElement.style.setProperty(
@@ -246,16 +267,27 @@ const lessTransClick = function () {
 		`${TRANSITION_TIME * 1000}ms`
 	);
 };
-lessTrans.addEventListener('click', lessTransClick);
 
-const showAndHide = function (e) {
-	if (e.code == 'KeyH') {
-		buttonContainer.classList.remove('move-to-front');
-		buttonContainer.classList.add('move-to-back');
-	} else {
-		buttonContainer.classList.remove('move-to-back');
-		buttonContainer.classList.add('move-to-front');
-	}
+//Initial setup sets the visible number equal to the config
+const init = function () {
+	clear(slideNumber);
+	slideNumber.innerHTML = SLIDE_DURATION;
+	clear(transNumber);
+	transNumber.innerHTML = TRANSITION_TIME;
+	document.documentElement.style.setProperty(
+		'--TRANSITION-TIME',
+		`${TRANSITION_TIME * 1000}ms`
+	);
 };
+
+init();
+
+//Clickables
+startBtn.addEventListener('click', start);
+
+moreSlide.addEventListener('click', moreSlideClick);
+lessSlide.addEventListener('click', lessSlideClick);
+moreTrans.addEventListener('click', moreTransClick);
+lessTrans.addEventListener('click', lessTransClick);
 
 document.addEventListener('keypress', showAndHide);
